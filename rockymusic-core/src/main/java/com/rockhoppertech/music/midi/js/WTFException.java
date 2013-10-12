d56e69f2f65b59689a280f2be8b5d903d3c6ebbd
Gene De Lisa <gene@rockhoppertech.com>
2013-10-12 17:26:28 -0400

    Added KeySignature

---
+++ rockymusic-core/src/main/java/com/rockhoppertech/music/midi/js/WTFException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.rockhoppertech.music.midi.js;
+
+/**
+ *
+ * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
+ *
+ */
+@SuppressWarnings("serial")
+public class WTFException extends RuntimeException {
+    public WTFException(String m) {
+        super(m);
+    }
+}
