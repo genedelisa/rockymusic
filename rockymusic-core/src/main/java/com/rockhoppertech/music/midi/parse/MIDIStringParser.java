dcfaca82ab3674cc4df0722c48b9998e7d22cf94
Gene De Lisa <gene@rockhoppertech.com>
2013-10-13 21:59:03 -0400

    brief tokens in append mode

--- rockymusic-core/src/main/java/com/rockhoppertech/music/midi/parse/MIDIStringParser.java
+++ rockymusic-core/src/main/java/com/rockhoppertech/music/midi/parse/MIDIStringParser.java
@@ -140,6 +140,7 @@ public class MIDIStringParser {
         MIDINote n = null;
         Scanner sc = new Scanner(s);
         sc.useDelimiter(",");
+        
 
         String p = null;
 
@@ -150,10 +151,11 @@ public class MIDIStringParser {
             p += this.runningOctave;
             logger.debug("short " + p);
         } else {
+        	String tok = sc.next();
         	sc.close();
             throw new IllegalArgumentException(String
                     .format("Bad pitch token '%s'",
-                            sc.next()));
+                            tok));
         }
 
         double start = 1d;
@@ -209,6 +211,59 @@ public class MIDIStringParser {
             int pb = sc.nextInt();
             n.setPitchBend(pb);
         }
+        sc.close();
+        return n;
+    }
+    /**
+     * Just the pitch and duration. The start beatw will be 1.
+     * Tell the track to be sequential if desired: track.sequential()
+     * 
+     * Format:
+     * <p>
+     *  Pitch
+     *  Pitch, Duration  
+     *  
+     * @param s
+     * @return
+     */
+    public MIDINote parseBriefNote(String s) {
+        MIDINote n = null;
+        Scanner sc = new Scanner(s);
+        sc.useDelimiter(",");
+        
+
+        String p = null;
+
+        if (sc.hasNext(fullPitchPattern)) {
+            p = sc.next();
+        } else if (sc.hasNext(shortPitchPattern)) {
+            p = sc.next();
+            p += this.runningOctave;
+            logger.debug("short " + p);
+        } else {
+        	sc.close();
+            throw new IllegalArgumentException(String
+                    .format("Bad pitch token '%s'",
+                            sc.next()));
+        }
+
+
+        double duration = 1d;
+        if (sc.hasNext(DurationParser.allPattern)) {
+            String tok = sc.next(DurationParser.allPattern);
+            logger.debug("token " + tok);
+            duration = DurationParser.getDuration(tok);
+        } else if (sc.hasNextDouble()) {
+            duration = sc.nextDouble();
+        }
+
+        n = new MIDINote(p, 1d, duration);
+        runningOctave = n.getPitch().getMidiNumber() / 12;
+        logger.debug(
+        String.format("p %s note %s ro %d",
+                          p,
+                          n,
+                          runningOctave));
         sc.close();
         return n;
     }
@@ -278,6 +333,8 @@ public class MIDIStringParser {
                     continue;
                 }
 
+                // S+ C5,.5
+                // S= C5, 1.0, .5
                 if (tok.startsWith("S") || tok.startsWith("s")) {
                     String op = tok.substring(1);
                     op = op.trim();
@@ -291,7 +348,7 @@ public class MIDIStringParser {
                             startBeat = Double.parseDouble(op);
                             if (logger.isDebugEnabled()) {
                                 String st = String
-                                        .format("Start op arg '%s'=%f\n",
+                                        .format("Start op arg '%s'=%f%n",
                                                 op,
                                                 startBeat);
                                 logger.debug(st);
@@ -334,8 +391,43 @@ public class MIDIStringParser {
                     continue;
                 }
 
-                MIDINote note = parseNote(tok);
+                MIDINote note = null;
 
+                if (startOp == StartMode.ADD) {
+                	//start beat is honored with add.
+                	// the start beat is chenged with append
+                	
+                	note = parseNote(tok);
+
+                	 if (startBeat != -1d) {
+                         note.setStartBeat(startBeat);
+                         if (logger.isDebugEnabled()) {
+                             String st = String
+                                     .format("Set start beat to %f for %s%n",
+                                             startBeat,
+                                             note);
+                             logger.debug(st);
+                         }
+                     }
+                    list.add(note);
+                    if (logger.isDebugEnabled()) {
+                        String st = String.format("added %s",
+                                                  note);
+                        logger.debug(st);
+                    }
+                } else if (startOp == StartMode.APPEND) {
+                	// since append ignores startbeat, there's no point in specifying it!
+                	// parseBrief is just pitch and duration.
+                	note = parseBriefNote(tok);
+                	
+                    list.append(note);
+                    if (logger.isDebugEnabled()) {
+                        String st = String.format("appended %s",
+                                                  note);
+                        logger.debug(st);
+                    }
+                }
+                
                 if (isRunningDurationSet) {
                     note.setDuration(runningDuration);
                 }
@@ -344,32 +436,6 @@ public class MIDIStringParser {
                 }
                 if (isRunningVelocitySet) {
                     note.setVelocity(runningVelocity);
-                }
-
-                if (startBeat != -1d) {
-                    note.setStartBeat(startBeat);
-                    if (logger.isDebugEnabled()) {
-                        String st = String
-                                .format("Set start beat to %f for %s\n",
-                                        startBeat,
-                                        note);
-                        logger.debug(st);
-                    }
-                }
-                if (startOp == StartMode.ADD) {
-                    list.add(note);
-                    if (logger.isDebugEnabled()) {
-                        String st = String.format("added %s",
-                                                  note);
-                        logger.debug(st);
-                    }
-                } else if (startOp == StartMode.APPEND) {
-                    list.append(note);
-                    if (logger.isDebugEnabled()) {
-                        String st = String.format("appended %s",
-                                                  note);
-                        logger.debug(st);
-                    }
                 }
             }
             sc.close();
