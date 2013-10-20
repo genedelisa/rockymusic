ad290c42245f84d77d03ebcbaf02fa18ba6e729f
Gene De Lisa <gene@rockhoppertech.com>
2013-10-20 06:30:01 -0400

    Initial

---
+++ rockymusic-core/src/main/java/com/rockhoppertech/collections/package-info.java
@@ -0,0 +1,30 @@
+/**
+ * There are many public open source projects that provide collections for Java. 
+ * I could not find a simple Circular List. Therefore I made one.
+ * If you know of a simpler better made version, let me know.
+ * 
+ *
+ * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
+ * @version 1.0
+ */
+package com.rockhoppertech.collections;
+
+/*
+ * #%L
+ * Rocky Music Core
+ * %%
+ * Copyright (C) 1996 - 2013 Rockhopper Technologies
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
