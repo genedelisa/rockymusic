19666af82a18af6a50af56776f45206f5fa8ae61
Gene De Lisa <gene@rockhoppertech.com>
2014-01-21 10:01:09 -0500

    Initial

---
+++ rockymusic-core/src/main/java/com/rockhoppertech/music/midi/js/Instrument.java
@@ -0,0 +1,222 @@
+package com.rockhoppertech.music.midi.js;
+
+import com.rockhoppertech.music.Pitch;
+import com.rockhoppertech.music.midi.gm.MIDIGMPatch;
+
+/**
+ * Instruments contain a GM patch, and a range.
+ * 
+ * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
+ * 
+ */
+public class Instrument {
+
+    /**
+     * 
+     */
+    public static final Instrument PIANO = new Instrument(MIDIGMPatch.PIANO,
+            MIDIGMPatch.PIANO.getName(), Pitch.A1, Pitch.C8);
+
+    public static final Instrument VIOLIN = new Instrument(MIDIGMPatch.VIOLIN,
+            MIDIGMPatch.VIOLIN.getName(), Pitch.G4, Pitch.A8);
+
+    public static final Instrument VIOLA = new Instrument(MIDIGMPatch.VIOLA,
+            MIDIGMPatch.VIOLA.getName(), Pitch.C4, Pitch.E7);
+
+    public static final Instrument CELLO = new Instrument(MIDIGMPatch.CELLO,
+            MIDIGMPatch.CELLO.getName(), Pitch.C3, Pitch.C7);
+
+    public static final Instrument CONTRABASS = new Instrument(
+            MIDIGMPatch.CONTRABASS,
+            MIDIGMPatch.CONTRABASS.getName(), Pitch.C3, Pitch.C6);
+
+    public static final Instrument HARP = new Instrument(MIDIGMPatch.HARP,
+            MIDIGMPatch.HARP.getName(), Pitch.B2, Pitch.GS8);
+
+    // TODO do the rest
+    // http://en.wikipedia.org/wiki/Range_(music)
+
+    // should I reference java sound's Instrument class?
+    // Soundbank soundbank = synth.getDefaultSoundbank();
+    // Instrument[] instr = soundbank.getInstruments();
+    
+    /**
+     * 
+     */
+    private MIDIGMPatch patch;
+    /**
+     * 
+     */
+    private String name;
+    /**
+     * 
+     */
+    private int minPitch;
+    /**
+     * 
+     */
+    private int maxPitch;
+
+    /**
+     * 
+     */
+    public Instrument() {
+
+    }
+
+    /**
+     * @param patch
+     * @param name
+     * @param minPitch
+     * @param maxPitch
+     */
+    public Instrument(MIDIGMPatch patch, String name, int minPitch, int maxPitch) {
+        this.patch = patch;
+        this.name = name;
+        this.minPitch = minPitch;
+        this.maxPitch = maxPitch;
+    }
+
+    /**
+     * @param pitch
+     * @return
+     */
+    public boolean isPitchInRange(int pitch) {
+        return (pitch >= this.minPitch && pitch <= this.maxPitch);
+    }
+
+    /**
+     * @param pitch
+     * @return
+     */
+    public boolean isPitchInRange(Pitch pitch) {
+        return (pitch.getMidiNumber() >= this.minPitch && pitch.getMidiNumber() <= this.maxPitch);
+    }
+
+    /**
+     * see Effective Java
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + maxPitch;
+        result = prime * result + minPitch;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((patch == null) ? 0 : patch.hashCode());
+        return result;
+    }
+
+    /*
+     * 
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Instrument other = (Instrument) obj;
+        if (maxPitch != other.maxPitch) {
+            return false;
+        }
+        if (minPitch != other.minPitch) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (patch != other.patch) {
+            return false;
+        }
+        return true;
+    }
+
+    /*
+     * 
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Instrument [patch=").append(patch).append(", name=")
+                .append(name).append(", minPitch=").append(minPitch)
+                .append(", maxPitch=").append(maxPitch).append("]");
+        return builder.toString();
+    }
+
+    /**
+     * @return the patch
+     */
+    public MIDIGMPatch getPatch() {
+        return patch;
+    }
+
+    /**
+     * @param patch
+     *            the patch to set
+     */
+    public void setPatch(MIDIGMPatch patch) {
+        this.patch = patch;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the minPitch
+     */
+    public int getMinPitch() {
+        return minPitch;
+    }
+
+    /**
+     * @param minPitch
+     *            the minPitch to set
+     */
+    public void setMinPitch(int minPitch) {
+        this.minPitch = minPitch;
+    }
+
+    /**
+     * @return the maxPitch
+     */
+    public int getMaxPitch() {
+        return maxPitch;
+    }
+
+    /**
+     * @param maxPitch
+     *            the maxPitch to set
+     */
+    public void setMaxPitch(int maxPitch) {
+        this.maxPitch = maxPitch;
+    }
+
+}
