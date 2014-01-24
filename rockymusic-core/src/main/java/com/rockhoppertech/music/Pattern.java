25bb8f85487b07aef25f2d76cc94ca75a741fdc7
Gene De Lisa <gene@rockhoppertech.com>
2014-01-24 14:45:18 -0500

    Initial import from old pattern classes

---
+++ rockymusic-core/src/main/java/com/rockhoppertech/music/Pattern.java
@@ -0,0 +1,605 @@
+/**
+ * 
+ */
+package com.rockhoppertech.music;
+
+/*
+ * #%L
+ * Rocky Music Core
+ * %%
+ * Copyright (C) 1996 - 2014 Rockhopper Technologies
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rockhoppertech.music.chord.Chord;
+import com.rockhoppertech.music.midi.js.MIDITrack;
+import com.rockhoppertech.music.midi.js.MIDITrackFactory;
+import com.rockhoppertech.music.scale.Scale;
+
+/**
+ * Essentially a holder for the values to create a MIDITrack based on a given
+ * set of degrees and a pattern contained in an int array.
+ * 
+ * The real work is done by {@link MIDITrackFactory#getTrackPattern()}
+ * 
+ * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
+ * 
+ */
+public class Pattern {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(Pattern.class);
+
+    /**
+     * Bloch's pattern and not the GoF pattern.
+     * 
+     * Pattern pattern = new
+     * Pattern.Builder(chord.getPitchClasses(),someInts).build();
+     * 
+     * or even
+     * 
+     * <pre>
+     * Pattern pattern = new Pattern.Builder(chord.getPitchClasses(), someInts)
+     *         .startPitch(C5).numOctaves(3).duration(Q)
+     *         .reverse(false).restBetweenPatterns(0).upAndDown(false)
+     *         .build();
+     * </pre>
+     * 
+     * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
+     * 
+     */
+    public static class Builder {
+        private int[] degrees;
+        private int[] pattern;
+        private int startPitch = Pitch.C5;
+        private int numOctaves = 1;
+        private double duration = Duration.Q;
+        private boolean reverse = false;
+        private boolean upAndDown = false;
+        private double restBetweenPatterns;
+
+        /**
+         * These are the non optional fields.
+         * 
+         * @param degrees
+         * @param pattern
+         */
+        public Builder(int[] degrees, int[] pattern) {
+            this.degrees = degrees;
+            this.pattern = pattern;
+        }
+
+        public Builder(List<Integer> d, int[] pattern) {
+            this.degrees = new int[d.size()];
+            for (int i = 0; i < degrees.length; i++) {
+                degrees[i] = d.get(i);
+            }
+            this.pattern = pattern;
+        }
+
+        public Pattern build() {
+            // enforced by the constructor.
+            if (this.degrees == null || this.pattern == null) {
+                throw new IllegalArgumentException(
+                        "You need both degrees and pattern");
+            }
+            Pattern result = new Pattern(this.degrees, this.pattern,
+                    this.startPitch, this.numOctaves, this.duration,
+                    this.reverse, this.restBetweenPatterns);
+            result.setUpAndDown(this.upAndDown);
+            this.reset();
+            return result;
+        }
+
+        private void reset() {
+            this.degrees = null;
+            this.pattern = null;
+            this.startPitch = Pitch.C5;
+            this.numOctaves = 1;
+            this.duration = Duration.Q;
+            this.reverse = false;
+            this.upAndDown = false;
+            this.restBetweenPatterns = 0;
+        }
+
+        public Builder degrees(int[] degrees) {
+            this.degrees = degrees;
+            return this;
+        }
+
+        public Builder pattern(int[] pattern) {
+            this.pattern = pattern;
+            return this;
+        }
+
+        public Builder startPitch(int startPitch) {
+            this.startPitch = startPitch;
+            return this;
+        }
+
+        public Builder numOctaves(int numOctaves) {
+            this.numOctaves = numOctaves;
+            return this;
+        }
+
+        public Builder duration(double duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public Builder restBetweenPatterns(double restBetweenPatterns) {
+            this.restBetweenPatterns = restBetweenPatterns;
+            return this;
+        }
+
+        public Builder reverse(boolean reverse) {
+            this.reverse = reverse;
+            return this;
+        }
+
+        public Builder upAndDown(boolean upAndDown) {
+            this.upAndDown = upAndDown;
+            return this;
+        }
+    }
+
+    static int[] spread(final int[] degrees, final int numOctaves) {
+        final int[] a = new int[degrees.length * numOctaves];
+        int j = 0;
+        for (int i = 0; i < numOctaves; i++) {
+            for (final int d : degrees) {
+                a[j++] = d + (i * 12);
+            }
+        }
+        return a;
+    }
+
+    private int[] degrees;
+    private int[] pattern;
+    private int startPitch;
+    private int numOctaves;
+    private double duration;
+    private boolean reverse;
+    private double restBetweenPatterns;
+    private String displayName;
+    private boolean upAndDown = false;
+    private List<Double> durationList;
+
+    private int limit;
+
+    public Pattern(final Chord chord, final int[] pattern,
+            final int startPitch, final int numOctaves, final double duration,
+            final boolean reverse, final double restBetweenPatterns) {
+
+        this(chord.getPitchClasses(), pattern, startPitch, numOctaves,
+                duration, reverse, restBetweenPatterns);
+    }
+
+    public Pattern(final int[] degrees, final int[] pattern) {
+        this(degrees, pattern, Pitch.C5, 1, Duration.Q, false, 0);
+    }
+
+    /**
+     * Each entry in the pattern is an index into the degrees. So this value
+     * must be < the length of the degree array.
+     * 
+     * A pattern = new int[] { 0 } is the identity result.
+     * 
+     * <pre>
+     * int[] degrees = new int[] { 0, 2, 4, 5, 6, 9, 11 };
+     * int[] pattern = new int[] { 0, 2 };
+     * int numOctaves = 1;
+     * double duration = Duration.Q;
+     * double restBetweenPatterns = Duration.Q;
+     * boolean reverse = false;
+     * Pattern nlPattern = new Pattern(degrees, pattern, C5, numOctaves, duration,
+     *         reverse, restBetweenPatterns);
+     * nlPattern.setUpAndDown(true);
+     * MIDITrack nl = nlPattern.createTrack(1d);
+     * </pre>
+     * 
+     * @param degrees
+     * @param pattern
+     * @param startPitch
+     * @param numOctaves
+     * @param duration
+     * @param reverse
+     * @param restBetweenPatterns
+     */
+    public Pattern(final int[] degrees, final int[] pattern,
+            final int startPitch, final int numOctaves, final double duration,
+            final boolean reverse, final double restBetweenPatterns) {
+
+        this.degrees = degrees;
+        this.startPitch = startPitch;
+        this.pattern = pattern;
+        this.numOctaves = numOctaves;
+        this.duration = duration;
+        this.reverse = reverse;
+        this.restBetweenPatterns = restBetweenPatterns;
+        final Date now = new Date();
+        final SimpleDateFormat df = new SimpleDateFormat(
+                "yyyy MMM dd kk:mm:ss:SSS");
+        this.setDisplayName(df.format(now));
+        // this.setDisplayName(DateFormat.getDateTimeInstance(DateFormat.SHORT,DateFormat.LONG).format(now));
+    }
+
+    public Pattern(final Scale scale, final int[] pattern) {
+        this(scale, pattern, Pitch.C5, 1, Duration.Q, false, Duration.Q);
+    }
+
+    public Pattern(final Scale scale, final int[] pattern, final int startPitch) {
+        this(scale, pattern, startPitch, 1, Duration.Q, false, Duration.Q);
+    }
+
+    /**
+     * <pre>
+     * Scale scale = ScaleFactory.getScaleByName(&quot;Major&quot;);
+     * int[] pattern = new int[] { 0, 2 };
+     * int numOctaves = 1;
+     * double duration = Duration.Q;
+     * double restBetweenPatterns = Duration.Q;
+     * boolean reverse = false;
+     * Pattern scalePattern = new Pattern(scale, pattern, C5, numOctaves, duration,
+     *         reverse, restBetweenPatterns);
+     * scalePattern.setUpAndDown(true);
+     * MIDITrack nl = scalePattern.createTrack(1d);
+     * </pre>
+     * 
+     * @param scale
+     * @param pattern
+     * @param startPitch
+     * @param numOctaves
+     * @param duration
+     * @param reverse
+     * @param restBetweenPatterns
+     */
+    public Pattern(final Scale scale, final int[] pattern,
+            final int startPitch, final int numOctaves, final double duration,
+            final boolean reverse, final double restBetweenPatterns) {
+
+        this(Interval.intervalsToDegrees(scale.getIntervals(), numOctaves),
+                pattern, startPitch, numOctaves, duration, reverse,
+                restBetweenPatterns);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Degrees: ").append(ArrayUtils.toString(this.degrees))
+                .append(' ');
+        sb.append("Pattern: ").append(ArrayUtils.toString(this.pattern))
+                .append(' ');
+        sb.append("Limit: ").append(this.limit).append(' ');
+        sb.append("Start Pitch: ").append(this.startPitch).append(' ');
+        sb.append("nOct: ").append(this.numOctaves).append(' ');
+        return sb.toString();
+    }
+
+    /**
+     * @param gap
+     *            the duration between patterns
+     * @return a MIDITrack containing the patterns
+     */
+    public MIDITrack createTrack() {
+        boolean spread = false;
+        return createTrack(0d, spread);
+    }
+
+    public static int max(int[] values) {
+        int max = Integer.MIN_VALUE;
+        for(int i : values) {
+            if( i > max)
+                max = i;
+        }
+        return max;
+          
+      }
+    /**
+     * If the pattern is based on degrees instead of midipitches then spread
+     * will project the degress over the number of octaves
+     * 
+     * @param gap
+     * @param spread
+     * @return
+     */
+    public MIDITrack createTrack(final double gap, boolean spread) {
+        if (this.degrees == null) {
+            throw new IllegalStateException("degrees are  null");
+        }
+
+        // this.limit = this.degrees.length - this.pattern.length + 1;
+        this.limit = this.degrees.length;
+        final int max = max(this.degrees);
+        
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(String.format("max %d limit %d", max, limit));
+        }
+        MIDITrack notelist = null;
+
+        if (spread) {
+            final int numOcts = (Pitch.MAX - this.startPitch) / 12 + 1;
+            final int[] a = Pattern.spread(this.degrees, numOcts);
+
+            notelist = MIDITrackFactory.getTrackPattern(a, this
+                    .getPattern(), this.limit, this.getStartPitch(), this
+                    .getNumOctaves(), this.getDuration(), this.isReverse(),
+                    this.getRestBetweenPatterns(), this.upAndDown);
+        } else {
+            List<Integer> midiNumberList = new ArrayList<Integer>();
+            // degrees is int[] and not Integer[]
+            // Collections.addAll(midiNumberList, this.degrees);
+            for (int d : this.degrees) {
+                midiNumberList.add(d);
+            }
+            List<Integer> patternList = new ArrayList<Integer>();
+            for (int d : this.pattern) {
+                patternList.add(d);
+            }
+            notelist = MIDITrackFactory.applyPattern(midiNumberList,
+                    patternList, this.getStartPitch(), this.getNumOctaves(),
+                    this.isReverse(), this.upAndDown).sequential();
+
+        }
+
+        String s = String.format("%s - %s - s%d o%d d%f r%b g%f u%b",
+                ArrayUtils.toString(this.degrees), ArrayUtils.toString(this
+                        .getPattern()), this.getStartPitch(), this
+                        .getNumOctaves(), this.getDuration(), this.isReverse(),
+                this.getRestBetweenPatterns(), this.upAndDown);
+        notelist.setName(s);
+        s = String
+                .format(
+                        "Generated from %s degrees.\nThe pattern applied was %s\nthe start pitch is %d\nnumber of octaves %d\nduration %f\nis reverse %b\ngap %f\nup and down %b\n",
+                        ArrayUtils.toString(this.degrees),
+                        ArrayUtils
+                                .toString(this.getPattern()),
+                        this
+                                .getStartPitch(),
+                        this.getNumOctaves(),
+                        this
+                                .getDuration(),
+                        this.isReverse(),
+                        this
+                                .getRestBetweenPatterns(),
+                        this.upAndDown);
+        notelist.setDescription(s);
+
+        // if (this.upAndDown) {
+        // if (this.getScale().isDescendingDifferent()) {
+        // MIDITrack reverse = ScaleFactory.createMIDITrack(this
+        // .getStartPitch() + 12, this.getScale()
+        // .getDescendingIntervals(), 1d, this.getDuration(), this
+        // .getNumOctaves(), true);
+        // notelist.append(reverse);
+        // notelist.sequential();
+        // } else {
+        // MIDITrack reverse = notelist.retrograde();
+        // notelist.append(reverse);
+        // notelist.sequential();
+        // }
+        // }
+
+        return notelist;
+    }
+
+    public MIDITrack createTrackWithLimit(final int limit) {
+        if (this.degrees == null) {
+            throw new IllegalStateException("degrees are  null");
+        }
+
+        // this.limit = this.degrees.length - this.pattern.length + 1;
+        final int max = max(this.degrees);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(String.format("max %d limit %d", max, limit));
+        }
+
+        // final int numOcts = (Pitch.MAX - this.startPitch) / 12 + 1;
+        final int numOcts = 2;
+
+        final int[] a = Pattern.spread(this.degrees, numOcts);
+
+        
+        final MIDITrack notelist = MIDITrackFactory.getTrackPattern(a,
+                this.getPattern(), this.limit, this.getStartPitch(), this
+                        .getNumOctaves(), this.getDuration(), this.isReverse(),
+                this.getRestBetweenPatterns(), this.upAndDown);
+
+        String s = String.format("%s - %s - s%d o%d d%f r%b g%f u%b",
+                ArrayUtils.toString(this.degrees), ArrayUtils.toString(this
+                        .getPattern()), this.getStartPitch(), this
+                        .getNumOctaves(), this.getDuration(), this.isReverse(),
+                this.getRestBetweenPatterns(), this.upAndDown);
+        notelist.setName(s);
+        s = String
+                .format(
+                        "Generated from %s degrees.\nThe pattern applied was %s\nthe start pitch is %d\nnumber of octaves %d\nduration %f\nis reverse %b\ngap %f\nup and down %b\n",
+                        ArrayUtils.toString(this.degrees),
+                        ArrayUtils
+                                .toString(this.getPattern()),
+                        this
+                                .getStartPitch(),
+                        this.getNumOctaves(),
+                        this
+                                .getDuration(),
+                        this.isReverse(),
+                        this
+                                .getRestBetweenPatterns(),
+                        this.upAndDown);
+        notelist.setDescription(s);
+
+        // if (this.upAndDown) {
+        // if (this.getScale().isDescendingDifferent()) {
+        // MIDITrack reverse = ScaleFactory.createMIDITrack(this
+        // .getStartPitch() + 12, this.getScale()
+        // .getDescendingIntervals(), 1d, this.getDuration(), this
+        // .getNumOctaves(), true);
+        // notelist.append(reverse);
+        // notelist.sequential();
+        // } else {
+        // MIDITrack reverse = notelist.retrograde();
+        // notelist.append(reverse);
+        // notelist.sequential();
+        // }
+        // }
+
+        return notelist;
+    }
+
+    /**
+     * @return the degrees
+     */
+    public int[] getDegrees() {
+        return this.degrees;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getDisplayName() {
+        return this.displayName;
+    }
+
+    /**
+     * @return the duration
+     */
+    public double getDuration() {
+        return this.duration;
+    }
+
+    /**
+     * @return the numOctaves
+     */
+    public int getNumOctaves() {
+        return this.numOctaves;
+    }
+
+    /**
+     * @return the pattern
+     */
+    public int[] getPattern() {
+        return this.pattern;
+    }
+
+    public String getPatternAsString() {
+        return ArrayUtils.toString(this.pattern);
+    }
+
+    /**
+     * @return the restBetweenPatterns
+     */
+    public double getRestBetweenPatterns() {
+        return this.restBetweenPatterns;
+    }
+
+    /**
+     * @return the startPitch
+     */
+    public int getStartPitch() {
+        return this.startPitch;
+    }
+
+    /**
+     * @return the reverse
+     */
+    public boolean isReverse() {
+        return this.reverse;
+    }
+
+    public boolean isUpAndDown() {
+        return this.upAndDown;
+    }
+
+    /**
+     * @param degrees
+     *            the degrees to set
+     */
+    public void setDegrees(final int[] degrees) {
+        this.degrees = degrees;
+    }
+
+    /**
+     * @param displayName
+     *            the displayName to set
+     */
+    public void setDisplayName(final String displayName) {
+        this.displayName = displayName;
+    }
+
+    /**
+     * @param duration
+     *            the duration to set
+     */
+    public void setDuration(final double duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * @param numOctaves
+     *            the numOctaves to set
+     */
+    public void setNumOctaves(final int numOctaves) {
+        this.numOctaves = numOctaves;
+    }
+
+    /**
+     * @param pattern
+     *            the pattern to set
+     */
+    public void setPattern(final int[] pattern) {
+        this.pattern = pattern;
+    }
+
+    /**
+     * @param restBetweenPatterns
+     *            the restBetweenPatterns to set
+     */
+    public void setRestBetweenPatterns(final double restBetweenPatterns) {
+        this.restBetweenPatterns = restBetweenPatterns;
+    }
+
+    /**
+     * @param reverse
+     *            the reverse to set
+     */
+    public void setReverse(final boolean reverse) {
+        this.reverse = reverse;
+    }
+
+    /**
+     * @param startPitch
+     *            the startPitch to set
+     */
+    public void setStartPitch(final int startPitch) {
+        this.startPitch = startPitch;
+    }
+
+    public void setUpAndDown(final boolean upAndDown) {
+        this.upAndDown = upAndDown;
+    }
+}
---
+++ rockymusic-core/src/main/java/com/rockhoppertech/music/PatternFactory.java
@@ -0,0 +1,148 @@
+package com.rockhoppertech.music;
+
+/*
+ * #%L
+ * Rocky Music Core
+ * %%
+ * Copyright (C) 1996 - 2014 Rockhopper Technologies
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static javax.swing.JOptionPane.*;
+
+/**
+ * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
+ * 
+ */
+public class PatternFactory {
+    private static final Logger logger = LoggerFactory
+            .getLogger(PatternFactory.class);
+
+    public static void main(String[] args) {
+
+        String s = (String) JOptionPane.showInputDialog(null, "Limit",
+                "Chooser", QUESTION_MESSAGE);
+        int limit = 7;
+        if (s != null) {
+            limit = Integer.parseInt(s);
+        }
+
+        Integer[] nums = new Integer[limit - 1];
+        for (int i = 2; i < nums.length + 2; i++) {
+            nums[i - 2] = i;
+        }
+        Integer size = (Integer) showInputDialog(null, "Pattern size",
+                "Chooser", QUESTION_MESSAGE, null, nums, nums[0]);
+
+        int w = JOptionPane.showConfirmDialog(null,
+                "Weed Repeats?", "choose one", JOptionPane.YES_NO_OPTION);
+
+        List<int[]> list = null;
+        list = PatternFactory.getPatterns(limit, size, w == YES_OPTION);
+        printArray(list);
+    }
+
+    /**
+     * @param patterns
+     */
+    private static void printArray(List<int[]> patterns) {
+        int num = 0;
+        for (int[] a : patterns) {
+            System.out.printf("%d:\t%s\n", num++, ArrayUtils.toString(a));
+        }
+    }
+
+    /**
+     * Calculate all the patterns of a given size up to a limit.
+     * 
+     * Example: limit 3, size 2:
+     * 
+     * <pre>
+     *     0 0 
+     *     0 1 
+     *     0 2 
+     *     1 0 
+     *     1 1 
+     *     1 2 
+     *     2 0 
+     *     2 1 
+     *     2 2
+     * </pre>
+     * 
+     * @param limit
+     *            the upper number of unique values.
+     * @param size
+     *            the length of each pattern.
+     * @param weedOutRepeats
+     *            remove patterns with consecutive values e.g. 0,0
+     * @return a List of integer arrays. Each array contains a pattern.
+     */
+    public static List<int[]> getPatterns(int limit, int size,
+            boolean weedOutRepeats) {
+        int num = (int) Math.pow(limit, size);
+        logger.debug("number of patterns is {}", num);
+        List<int[]> patterns = new ArrayList<int[]>(num);
+        for (int i = 0; i < num; i++) {
+            int[] a = new int[size];
+            int count = size - 1;
+            int b = 0;
+            while (count >= 0) {
+                a[count] = (int) ((i) / Math.pow(limit, b++) % limit);
+                count--;
+            }
+
+            if (weedOutRepeats) {
+                if (!hasRepeats(a)) {
+                    patterns.add(a);
+                }
+            } else {
+                patterns.add(a);
+            }
+        }
+        return patterns;
+    }
+
+    public static List<int[]> getPatterns(int limit, int size) {
+        return getPatterns(limit, size, false);
+    }
+
+    /**
+     * Checks to determine if there are consecutive values. e.g. 0,0 1,1 2,2,2,2
+     * etc.
+     * 
+     * @param a
+     * @return
+     */
+    public static boolean hasRepeats(int[] a) {
+        boolean ret = false;
+        for (int x = 1; x < a.length; x++) {
+            if (a[x] == a[x - 1]) {
+                return true;
+            }
+        }
+        return ret;
+    }
+
+}
