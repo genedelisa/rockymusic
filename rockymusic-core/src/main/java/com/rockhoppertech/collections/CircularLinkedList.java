8698ad76a8875012442e1cc57d17c782ab5d686d
Gene De Lisa <gene@rockhoppertech.com>
2014-01-27 13:31:17 -0500

    added constructor that takes a collection

--- rockymusic-core/src/main/java/com/rockhoppertech/collections/CircularLinkedList.java
+++ rockymusic-core/src/main/java/com/rockhoppertech/collections/CircularLinkedList.java
@@ -35,37 +35,121 @@ public class CircularLinkedList<E> extends LinkedList<E> implements
      * 
      */
 	private static final long serialVersionUID = -7528652646990925519L;
+	/**
+	 * 
+	 */
+	private int index = 0;
+	  /**
+     * 
+     */
+    private boolean first = true;
 
+    /**
+     * 
+     */
+    private boolean last = false;
+	
 	public CircularLinkedList() {
 	}
 
 	public CircularLinkedList(Collection<? extends E> c) {
 		super(c);
 	}
+	 public CircularLinkedList(final E[] array) {
+	        for (final E o : array) {
+	            this.add(o);
+	        }
+	    }
 
-	private int index = 0;
+	 /**
+	     * You must call previous or next before calling this. Perhaps "wasFirst"
+	     * would be a better name but the JavaBean convention differs.
+	     * 
+	     * @return a boolean
+	     */
+	    public final boolean isFirst() {
+	        return this.first;
+	    }
 
-	@Override
-	public E next() {
-		if (this.index == size()) {
-			this.index = 0;
-		}
-		return get(this.index++);
-	}
+	    /**
+	     * @return whether the collection is pointing at the last element.
+	     */
+	    public final boolean isLast() {
+	        return this.last;
+	    }
 
-	@Override
-	public E previous() {
-		if (--this.index < 0) {
-			this.index = size() - 1;
-		}
-		return get(this.index);
-	}
+	    /**
+	     * @return the next element
+	     * @see com.rockhoppertech.collections.CircularList#next()
+	     */
+	    @Override
+	    public final E next() {
+	        if (this.index == size()) {
+	            this.index = 0;
+	        }
 
-	@Override
-	public void reset() {
-		this.index = 0;
-	}
+	        E result = get(this.index++);
+	        this.updateFirstLast();
 
+	        return result;
+	    }
+
+	    /**
+	     * @return the previous element
+	     * @see com.rockhoppertech.collections.CircularList#previous()
+	     */
+	    @Override
+	    public final E previous() {
+	        if (--this.index < 0) {
+	            this.index = size() - 1;
+	        }
+	        this.updateFirstLast();
+	        return get(this.index);
+	    }
+	    
+    // @Override
+    // public E next() {
+    // if (this.index == size()) {
+    // this.index = 0;
+    // }
+    // return get(this.index++);
+    // }
+    //
+    // @Override
+    // public E previous() {
+    // if (--this.index < 0) {
+    // this.index = size() - 1;
+    // }
+    // return get(this.index);
+    // }
+
+    /**
+     * @see com.rockhoppertech.collections.CircularList#reset()
+     */
+    @Override
+    public final void reset() {
+        this.index = 0;
+        this.first = true;
+        this.last = false;
+    }
+
+    /**
+     * 
+     */
+    private void updateFirstLast() {
+        if (this.index - 1 == size() - 1) {
+            this.last = true;
+        } else {
+            this.last = false;
+        }
+        if (this.index == 0) {
+            this.first = true;
+        } else {
+            this.first = false;
+        }
+    }
+    
+	/*
 	public static void main(String[] args) {
 		CircularLinkedList<Integer> cal = new CircularLinkedList<Integer>();
 		cal.add(1);
@@ -80,5 +164,6 @@ public class CircularLinkedList<E> extends LinkedList<E> implements
 			System.out.println(cal.previous());
 		}
 	}
+	*/
 
 }
