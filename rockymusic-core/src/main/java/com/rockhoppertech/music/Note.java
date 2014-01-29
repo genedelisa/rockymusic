724081a8b3553004d287dd69b05fcdc5ec64f5fc
Gene De Lisa <gene@rockhoppertech.com>
2014-01-29 18:29:13 -0500

    fixed beat comparator

--- rockymusic-core/src/main/java/com/rockhoppertech/music/Note.java
+++ rockymusic-core/src/main/java/com/rockhoppertech/music/Note.java
@@ -65,18 +65,17 @@ public class Note implements Comparable<Note>, Timed, Serializable {
      * 
      * 
      * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
-     * @version $Revision$, $Date$
      * @since 1.0
      * @see Comparator
      */
-    class BeatComparator implements Comparator<Note> {
+    static class BeatComparator implements Comparator<Note> {
         @Override
         public int compare(final Note o, final Note o2) {
             final double t1 = o.getStartBeat();
             final double t2 = o2.getStartBeat();
             if (t1 < t2) {
                 return -1;
-            } else if (t1 == t2) {
+            } else if ( Math.abs(t1 - t2) < .0000001 ) {                
                 return 0;
             } else if (t1 > t2) {
                 return 1;
