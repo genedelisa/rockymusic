112063020cd8c3d73ce0a1a1a1c6ddf007e8a41a
Gene De Lisa <gene@rockhoppertech.com>
2014-01-31 23:02:20 -0500

    Made beatcomparator a top level class.

--- rockymusic-core/src/main/java/com/rockhoppertech/music/Note.java
+++ rockymusic-core/src/main/java/com/rockhoppertech/music/Note.java
@@ -38,7 +38,6 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.Serializable;
 import java.text.NumberFormat;
-import java.util.Comparator;
 
 import org.apache.commons.lang3.Range;
 import org.slf4j.Logger;
@@ -61,40 +60,12 @@ import com.rockhoppertech.music.midi.js.MIDINote;
  * @see MIDINote
  */
 public class Note implements Comparable<Note>, Timed, Serializable {
-    /**
-     * Class <code>BeatComparator</code> compares start beats.
-     * 
-     * 
-     * @author <a href="mailto:gene@rockhoppertech.com">Gene De Lisa</a>
-     * @since 1.0
-     * @see Comparator
-     */
-    static class BeatComparator implements Comparator<Note>, Serializable {
-        /**
-         * Serializaiton.
-         */
-        private static final long serialVersionUID = 1701446492454684836L;
-
-        @Override
-        public int compare(final Note o, final Note o2) {
-            final double t1 = o.getStartBeat();
-            final double t2 = o2.getStartBeat();
-            if (t1 < t2) {
-                return -1;
-            } else if (Math.abs(t1 - t2) < .0000001) {
-                return 0;
-            } else if (t1 > t2) {
-                return 1;
-            }
-            return 0;
-        }
-    }
 
     /**
-     * 
+     * For Serialization.
      */
-    private static final long serialVersionUID = 1L;
-
+    private static final long serialVersionUID = -6744900968063285081L;
+    
     private static final Logger logger = LoggerFactory.getLogger(Note.class);
     private Pitch pitch;
     private double startBeat;
