3bdfa005418e03eebb1838c530fee53decda31c1
Gene De Lisa <gene@rockhoppertech.com>
2014-02-07 13:26:34 -0500

    mvn inserted the license info.

--- rockymusic-examples/src/main/java/com/rockhoppertech/music/examples/Guava.java
+++ rockymusic-examples/src/main/java/com/rockhoppertech/music/examples/Guava.java
@@ -20,18 +20,27 @@ package com.rockhoppertech.music.examples;
  * #L%
  */
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeSet;
 import com.google.common.collect.TreeRangeSet;
+import com.rockhoppertech.music.Pitch;
 import com.rockhoppertech.music.midi.js.MIDINote;
 import com.rockhoppertech.music.midi.js.MIDITrack;
 import com.rockhoppertech.music.midi.js.MIDITrackBuilder;
+import com.rockhoppertech.music.midi.js.modifiers.google.PitchGreaterThanPredicate;
+import com.rockhoppertech.music.midi.js.modifiers.google.PitchLessThanPredicate;
 
 /**
  * Guava playground.
@@ -89,6 +98,74 @@ public class Guava {
             note.setMidiNumber(note.getMidiNumber() + this.value);
             return note;
         }
+    }
+    
+    public static void funpred() {
+        MIDITrack track = MIDITrackBuilder.create()
+                .noteString("C5 CS F FS E F4")
+                .build();
+
+        final Predicate<MIDINote> trebleNote = new Predicate<MIDINote>() {
+            public boolean apply(MIDINote n) {
+                return n.getMidiNumber() > Pitch.C5;
+            }
+        };
+        List<MIDINote> highNotes = Lists.newArrayList(Iterables.filter(track,
+                trebleNote));
+        // for (MIDINote note : highNotes) {
+        // System.out.println(note);
+        // }
+
+        Predicate<MIDINote> high = new PitchGreaterThanPredicate(Pitch.C5);
+        highNotes = Lists.newArrayList(Iterables.filter(track, high));
+        for (MIDINote note : highNotes) {
+            System.out.println(note);
+        }
+
+        // final List<MIDINote> highNotes =
+        // Lists.immutableList(Iterables.filter(list, trebleNote));
+        // ImmutableList<MIDINote> imlist =
+        // ImmutableList.copyOf(Iterables.filter(list, high));
+        //
+        //
+        final Function<MIDINote, MIDINote> transposeFunction = new Function<MIDINote, MIDINote>() {
+            public MIDINote apply(MIDINote n) {
+                n.setMidiNumber(n.getMidiNumber() + 1);
+                return n;
+            }
+        };
+        // Iterable<MIDINote> mni = Iterables.transform(Iterables.filter(list,
+        // high), fun);
+
+        final Function<MIDINote, Integer> tonums = new Function<MIDINote, Integer>() {
+            @Override
+            public Integer apply(MIDINote n) {
+                return n.getMidiNumber();
+            }
+        };
+
+        ImmutableList<Integer> imInts = ImmutableList.copyOf(Iterables
+                .transform(Iterables.filter(track, high), tonums));
+        for (Integer in : imInts) {
+            System.out.println(in);
+        }
+
+        // Iterable i = Iterables.transform(Iterables.filter(Iterables.filter(
+        // list, MIDINote.class), high), nameForNumber);
+
+        Predicate<MIDINote> and = Predicates.and(new PitchGreaterThanPredicate(
+                Pitch.C5), new PitchLessThanPredicate(Pitch.F5));
+        ImmutableList<MIDINote> bandpass = ImmutableList.copyOf(Iterables
+                .filter(track, and));
+        for (MIDINote in : bandpass) {
+            System.out.println(in);
+        }
+
+        
+        Predicate<MIDINote> compose = Predicates.compose(
+                new PitchGreaterThanPredicate(
+                        Pitch.C5),
+                transposeFunction);
     }
 
 }
