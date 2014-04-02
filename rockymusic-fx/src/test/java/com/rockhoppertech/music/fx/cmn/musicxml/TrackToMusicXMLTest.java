26793f69fbc96205794d0aedc027477a04a6fe58
Gene De Lisa <gene@rockhoppertech.com>
2014-04-02 09:34:20 -0400

    added license header

--- rockymusic-fx/src/test/java/com/rockhoppertech/music/fx/cmn/musicxml/TrackToMusicXMLTest.java
+++ rockymusic-fx/src/test/java/com/rockhoppertech/music/fx/cmn/musicxml/TrackToMusicXMLTest.java
@@ -1,5 +1,25 @@
 package com.rockhoppertech.music.fx.cmn.musicxml;
 
+/*
+ * #%L
+ * Rocky Music FX
+ * %%
+ * Copyright (C) 1996 - 2014 Rockhopper Technologies
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.rockhoppertech.music.midi.js.MIDITrack;
 import com.rockhoppertech.music.midi.js.MIDITrackBuilder;
 import org.junit.Test;
